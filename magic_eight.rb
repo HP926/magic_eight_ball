7da39d4cd309f62c64d377b03e737a473b97e656
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-05-28 12:02:44 -0600

    created class user_question

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,2 +1,18 @@
 class MagicEight
-end
+
+  def user_question
+    puts "What's your question?"
+    user_answer = gets.chomp
+  end
+ end 
+ 
+
+
+
+
+
+
+
+
+clone = MagicEight.new
+clone.user_question
