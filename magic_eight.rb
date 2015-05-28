a7c0ff415563fba764b09bad94d3d1d2a26fff3a
Doyi Lee <doyilee@hotmail.com>
2015-05-28 12:24:32 -0600

    added question options

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,18 +1,27 @@
+require "pry-byebug"
+
 class MagicEight
 
   def user_question
     puts "What's your question?"
-    user_answer = gets.chomp
+    user_question = gets.chomp
   end
- end 
+
+  # binding.pry
+
+	def initialize() 
+		@answer_list = [
+		"Yes, of course!", "No, never.", "I don't know", "Maybe? You'll see."
+		]
+  end
+
+  def choose_answer
+  	sleep 0.5
+  	puts @answer_list.sample(1)
+  end
+end 
  
-
-
-
-
-
-
-
 
 clone = MagicEight.new
 clone.user_question
+clone.choose_answer
